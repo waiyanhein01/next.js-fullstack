cca47f053afc13370940fb386abb865d95a878ff
waiyanhein01 <wyan00196@gmail.com>
2025-11-28 23:29:55 +0630

    fix : prisma error and error page

--- next-course/app/(root)/blog/error.tsx
+++ next-course/app/(root)/blog/error.tsx
@@ -4,6 +4,7 @@ const Error = ({ error }: { error: Error & { digest?: string } }) => {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center">
       <h1>Something went wrong!</h1>
+      <p className="">Author page can't be found</p>
       {/* <p>{error.message}</p> */}
     </div>
   );
