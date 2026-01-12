8260e5687f2da525ba7910c2a1ad8fc8cf511b2c
waiyanhein01 <wyan00196@gmail.com>
2026-01-12 10:59:34 +0630

    learn : 96 to 101

---
+++ next-cache/components/posts/PostComponent.tsx
@@ -0,0 +1,31 @@
+import { cacheLife } from "next/cache";
+
+interface Post {
+  author: string;
+  category: string;
+  content: string;
+  date: string;
+  id: number;
+  title: string;
+}
+
+const PostComponent = async () => {
+  "use cache";
+  cacheLife("hours");
+
+  const res = await fetch("https://api.vercel.app/blog");
+  const posts = await res.json();
+  return (
+    <div>
+      <ul>
+        {posts.slice(0, 5).map((post: Post) => (
+          <li key={post.id}>
+            <h1>{post.title}</h1>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default PostComponent;
