1c14c5c537a7130b2dead86ac569946f16db32e0
waiyanhein01 <wyan00196@gmail.com>
2026-02-04 16:49:49 +0630

    learn : 140-142 Proxy & Metadata

--- next-cache/next.config.ts
+++ next-cache/next.config.ts
@@ -3,6 +3,20 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   cacheComponents: true,
   reactCompiler: true,
+  redirects() {
+    return [
+      {
+        source: "/blog/:path*",
+        destination: "/posts/:path*",
+        permanent: false,
+      },
+      {
+        source: "/old-blog",
+        destination: "/posts",
+        permanent: true, //301 redirect
+      },
+    ];
+  },
 };
 
 export default nextConfig;
--- next-cache/proxy.ts
+++ next-cache/proxy.ts
@@ -2,7 +2,9 @@ import { NextRequest, NextResponse } from "next/server"
 
 export const proxy = (request: NextRequest) => {
     const pathname = request.nextUrl.pathname
-    console.log("proxy", pathname)
+    if (pathname.startsWith("/blog")) {
+        return NextResponse.redirect(new URL("/posts", request.url))
+    }
     return NextResponse.next()
 }
 
@@ -10,5 +12,9 @@ export const config = {
     matcher:
         '/((?!api|_next/data|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)',
 }
+
+// export const config = {
+//     matcher: ["/old-blog/:path*"],
+// }
 
 // Proxy will still run for /_next/data/* routes despite being excluded
