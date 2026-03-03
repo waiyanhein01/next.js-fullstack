ad34c7b3ed69588627eef2fbe6832d68c6b31d48
waiyanhein01 <wyan00196@gmail.com>
2026-03-03 16:30:03 +0630

    learn : 157 to 159

---
+++ global-shop/lib/auth-client.ts
@@ -0,0 +1,10 @@
+import { createAuthClient } from "better-auth/react"
+import { emailOTPClient } from "better-auth/client/plugins"
+
+export const { signIn, signUp, useSession, emailOtp } = createAuthClient({
+    /** The base URL of the server (optional if you're using the same domain) */
+    baseURL: "http://localhost:3000",
+    plugins: [
+        emailOTPClient()
+    ]
+})
--- global-shop/lib/auth.ts
+++ global-shop/lib/auth.ts
@@ -9,4 +9,10 @@ export const auth = betterAuth({
     emailAndPassword: {
         enabled: true,
     },
+    socialProviders: {
+        google: {
+            clientId: process.env.GOOGLE_CLIENT_ID as string,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+        },
+    },
 });
